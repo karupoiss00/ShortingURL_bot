e45097734e0791c49eecaeac8467d4e3f2cf2922
karupoiss00 <glizerin1108@gmail.com>
2019-06-26 20:16:51 +0400

    Добавлено приветствие бота.

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
-	echo 'test output'
+	include('vendor/autoload.php'); //Подключаем библиотеку
+    use Telegram\Bot\Api;
+
+    $telegram = new Api('885752742:AAF63rND57OidzVAJ3ReDp7qGkX7oVaunBY'); //Устанавливаем токен, полученный у BotFather
+    $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
+
+    $text = $result["message"]["text"]; //Текст сообщения
+    $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
+    $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
+    $keyboard = [["Сократить ссылку"],["Расшифровать ссылку"]]; //Клавиатура
+
+    if($text)
+    {
+        if ($text == "/start") 
+        {
+            if (strlen($name) != 0) 
+            {
+                $reply = "Добро пожаловать, ".$name."!";
+            }
+            else 
+            {
+                $reply = "Добро пожаловать, Незнакомец";
+            }
+            $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+        }
+        elseif ($text == "/help")
+        {
+            $reply = "Даннный бот прдназначен для сокращения и расшифровки сокращенныйх ссылок при помощи сервиса goo.gl.\n Чтобы воспользоваться ботом, используйте клавиатуру.";
+            $telegram->sendMessagе([ 'chat_id' => $chat_id, 'text' => $reply ]);
+        }
+    }
+    else
+    {
+    	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+    }
 ?>
