162e21120bfd3f6f3a9d3e0dcdd1c8f688676072
karupoiss00 <glizerin1108@gmail.com>
2019-06-30 18:10:43 +0300

    Добавлена запись действий пользователя в БД. Добавлен вывод истории. Функции вынесены в отдельный файл.

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
 	include('vendor/autoload.php'); //Подключаем библиотеку
+	include('url_handler.php'); 
 	use Telegram\Bot\Api;
-	
-	const LOGIN =  'o_4dkf0dhc6p';
-	const API_KEY =  'R_a9dbe6c319fe4397946c86b8798b7abb';
-	const END_POINT =  'http://api.bit.ly/v3';
-	const HELP_REPLY = 'Данный бот предназначен для работы со ссылками. Он умеет сокращать или расшифровывать уже сокращённые ссылки. Для того, чтобы воспользоваться ботом, отправьте ему ссылку, которую необходимо сократить.';
-	const DB_HOST = 'us-cdbr-iron-east-02.cleardb.net';
-	const DB_USER = 'b8ee931ac989b3';
-	const DB_PASS = '672ff549';
-	const DB_NAME = 'heroku_3fd1c3b404a32b0';
 	
 	$telegram = new Api('885752742:AAF63rND57OidzVAJ3ReDp7qGkX7oVaunBY');
 	$result = $telegram->getWebhookUpdates();
@@ -37,77 +29,39 @@
 			if (strpos($text, 'http') === FALSE) {
 				$text = 'http://'.$text;
 			}
+			
 			if (strpos($text, 'bit.ly') === FALSE) {
-				$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => getShortUrl($text)]);
+				$reply = getShortUrl($text);
 			}
 			else {
-				$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => getLongUrl($text)]);
+				$reply = getLongUrl($text);	
+			}
+			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
+			if ($reply != 'Ссылка некорректна') {
+				$db->where('chat_id', $chat_id);
+				$record = $db->getOne('user_request_history');
+				if (count($record)) {	
+					$record['first_request'] = $record['second_request'];
+					$record['second_request'] = $record['third_request'];
+					$record['third_request'] = $record['fourth_request'];
+					$record['fourth_request'] = $record['fifth_request'];
+					$record['fifth_request'] = $reply;
+					$db->update('user_request_history', $record);
+				}
+				else {
+					$data = [
+						'chat_id' => $chat_id,
+						'first_request' => ' <пусто>',
+						'second_request' => '<пусто>',
+						'third_request' => '<пусто>',
+						'fourth_request' => '<пусто>',
+						'fifth_request' => $reply
+					];
+					$db->insert('user_request_history', $data);
+				}
 			}
         }
     }
 	else {
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => 'Отправьте текстовое сообщение.' ]);
-	}
-
-    function getShortUrl($longUrl):string {
-        $url = "http://api.bit.ly/shorten?version=2.0.1&longUrl=$longUrl&login=o_4dkf0dhc6p&apiKey=R_a9dbe6c319fe4397946c86b8798b7abb&format=json&history=1";
-        $s = curl_init();
-        curl_setopt($s,CURLOPT_URL, $url);
-        curl_setopt($s,CURLOPT_HEADER,false);
-        curl_setopt($s,CURLOPT_RETURNTRANSFER,1);
-        $result = curl_exec($s);
-        curl_close( $s );
-        $obj = json_decode($result, true);
-        $res = $obj["results"]["$longUrl"]["shortUrl"];
-        if (strlen($res) != 0) {
-			insertHistoryRecord($res);
-            return $res;
-        }
-        else {
-            return 'Ссылка некорректна';
-        }
-    }
-	
-	function getLongUrl($shortUrl):string {
-		$query = http_build_query(
-			array(
-				'login' => LOGIN,
-				'apiKey' => API_KEY,
-				'shortUrl' => $shortUrl,
-				'format' => 'txt'
-			)
-		);
-		$res = file_get_contents(sprintf('%s/%s?%s', END_POINT, 'expand', $query));
-		if ($res == 'NOT_FOUND') {
-			return "Ссылка не найдена";
-		}
-		else { 	
-			insertHistoryRecord($res);
-			return $res;
-		}
-	}
-	
-	function insertHistoryRecord($record) {
-		$db->where ('chat_id', $chat_id);
-		
-		$history = array
-		(
-			$db->getOne ("first_request"),
-			$db->getOne ("second_request"),
-			$db->getOne ("third_request"),
-			$db->getOne ("fouth_request"),
-			$db->getOne ("fifth_request")
-		);
-		array_shift($history);
-		$history[4] = $record;
-		
-		$data = array 
-		(
-			"first_request" => $history[0],
-			"second_request" => $history[1],
-			"third_request" => $history[2],
-			"fouth_request" => $history[3],
-			"fifth_request" => $history[4]
-		);
-		$db->insert ('users', $data);
 	}
