686a3ceb1f4a983e4db736a6d9caf6988da625db
karupoiss00 <glizerin1108@gmail.com>
2019-07-01 16:37:48 +0300

    upd

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 			else {
 				$data = [
 					'chat_id' => $chat_id,
-					'first_request' => '<пусто>',
+					'first_request' => ' <пусто>',
 					'second_request' => '<пусто>',
 					'third_request' => '<пусто>',
 					'fourth_request' => '<пусто>',
@@ -71,30 +71,27 @@
 				$reply = getLongUrl($text);	
 			}
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
-			if ($reply != 'Ссылка некорректна') {
-				$db->where('chat_id', $chat_id);
-				$record = $db->getOne('user_request_history');
-				if (count($record)) {	
-					$record['first_request'] = $record['second_request'];
-					$record['second_request'] = $record['third_request'];
-					$record['third_request'] = $record['fourth_request'];
-					$record['fourth_request'] = $record['fifth_request'];
-					$record['fifth_request'] = $reply;
-					$db->update('user_request_history', $record);
-				}
-				else {
-					$data = [
-						'chat_id' => $chat_id,
-						'first_request' => ' <пусто>',
-						'second_request' => '<пусто>',
-						'third_request' => '<пусто>',
-						'fourth_request' => '<пусто>',
-						'fifth_request' => $reply
-					];
-					$db->insert('user_request_history', $data);
-				}
+			$db->where('chat_id', $chat_id);
+			$record = $db->getOne('user_request_history');
+			if (count($record)) {
+				$record['first_request'] = $record['second_request'];
+				$record['second_request'] = $record['third_request'];
+				$record['third_request'] = $record['fourth_request'];
+				$record['fourth_request'] = $record['fifth_request'];
+				$record['fifth_request'] = $reply;
+				$db->update('user_request_history', $record);
 			}
-			
+			else {
+				$data = [
+					'chat_id' => $chat_id,
+					'first_request' => '<пусто>',
+					'second_request' => '<пусто>',
+					'third_request' => '<пусто>',
+					'fourth_request' => '<пусто>',
+					'fifth_request' => $reply
+				];
+				$db->insert('user_request_history', $data);
+			}		
 		}
 	}
 	else {
