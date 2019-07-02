d8b9c735eefce29238a0af58ab719808e980f703
karupoiss00 <glizerin1108@gmail.com>
2019-07-02 14:27:59 +0300

    Вынесена функция обновления истории

--- db_handler.php
+++ db_handler.php
@@ -13,3 +13,27 @@
 	
 	$db = new MysqliDb (DB_HOST, DB_USER, DB_PASS, DB_NAME);
 	$db->autoReconnect = true;
+	
+	function updateHistory($lastAction, $userId) {
+		$db->where('chat_id', $userId);
+		$record = $db->getOne('user_request_history');
+		if (count($record)) {
+			$record[FIRST_REQUEST] = $record[SECOND_REQUEST];
+			$record[SECOND_REQUEST] = $record[THIRD_REQUEST];
+			$record[THIRD_REQUEST] = $record[FOURTH_REQUEST];
+			$record[FOURTH_REQUEST] = $record[FIFTH_REQUEST];
+			$record[FIFTH_REQUEST] = $lastAction;
+			$db->update('user_request_history', $record);
+		}
+		else {
+			$data = [
+				CHAT_ID => $chat_id,
+				FIRST_REQUEST => '<пусто>',
+				SECOND_REQUEST => '<пусто>',
+				THIRD_REQUEST => '<пусто>',
+				FOURTH_REQUEST => '<пусто>',
+				FIFTH_REQUEST => $lastAction
+			];
+			$db->insert('user_request_history', $data);
+		}
+	}
