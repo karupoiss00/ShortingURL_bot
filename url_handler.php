c89ffe48e8ce5cf12fe8b655650e6774169a6eaa
karupoiss00 <glizerin1108@gmail.com>
2019-07-02 22:47:19 +0400

    Исправлены мелкие баги.

--- url_handler.php
+++ url_handler.php
@@ -3,6 +3,7 @@
 	const API_KEY =  'R_a9dbe6c319fe4397946c86b8798b7abb';
 	const END_POINT =  'http://api.bit.ly/v3';
 	const ERROR_MESS = 'Ссылка некорректна';
+	const HELP_REPLY = 'Данный бот предназначен для работы со ссылками. Он умеет сокращать или расшифровывать уже сокращённые ссылки. Для того, чтобы воспользоваться ботом, отправьте ему ссылку, которую необходимо сократить. Для просмотра последних действий, воспользуйтесь командой /history';
 	
 	function getShortUrl($longUrl):string {
         $url = "http://api.bit.ly/shorten?version=2.0.1&longUrl=$longUrl&login=o_4dkf0dhc6p&apiKey=R_a9dbe6c319fe4397946c86b8798b7abb&format=json&history=1";
