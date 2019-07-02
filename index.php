a472f50bbad0af055b704b63e639090b2f9ba034
karupoiss00 <glizerin1108@gmail.com>
2019-07-02 12:05:53 +0300

    Произведен рефакторинг кода. Работа с БД вынесена в отдельный файл.

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 	include('vendor/autoload.php');
 	include('url_handler.php');
-	use Telegram\Bot\Api;
+	include('globals.php');
+	include('db_handler.php.');
 	
 	$telegram = new Api('885752742:AAF63rND57OidzVAJ3ReDp7qGkX7oVaunBY');
 	$result = $telegram->getWebhookUpdates();
 	$text = $result["message"]["text"];
 	$chat_id = $result["message"]["chat"]["id"];
 	$name = $result["message"]["from"]["first_name"];
-	$db = new MysqliDb (DB_HOST, DB_USER, DB_PASS, DB_NAME);
-	$db->autoReconnect = true;
 	
+	use Telegram\Bot\Api;
 	
 	if($text) {
 		if ($text == '/start') {
@@ -27,37 +27,8 @@
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
 		}
 		elseif ($text == '/history') {
-			$db->where('chat_id', $chat_id);
-			$res = $db->getOne('user_request_history');
-			if (count($res)) {
-				$history = array_slice($res , 1);
-				$reply = 'Последние действия:
-				 ';
-				foreach ($history as $record) {
-					$reply .= $record.'
-					';
-				}
-				$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-			}
-			else {
-				$data = [
-					'chat_id' => $chat_id,
-					'first_request' => '<пусто>',
-					'second_request' => '<пусто>',
-					'third_request' => '<пусто>',
-					'fourth_request' => '<пусто>',
-					'fifth_request' => '<пусто>'
-				];
-				$db->insert('user_request_history', $data);
-				$reply = 'Последние действия:
-				 ';
-				$history = array_slice($data , 1);
-				foreach ($history as $record) {
-					$reply .= $record.'
-					';
-				}
-				$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-			}
+			$reply = getUserHistory($chat_id);
+			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
 		}
 		else {
 			if (strpos($text, 'http') === FALSE) {
@@ -72,27 +43,7 @@
 			}
 			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
 			if ($reply != 'Ссылка некорректна') {
-				$db->where('chat_id', $chat_id);
-				$record = $db->getOne('user_request_history');
-				if (count($record)) {
-					$record['first_request'] = $record['second_request'];
-					$record['second_request'] = $record['third_request'];
-					$record['third_request'] = $record['fourth_request'];
-					$record['fourth_request'] = $record['fifth_request'];
-					$record['fifth_request'] = $reply;
-					$db->update('user_request_history', $record);
-				}
-				else {
-					$data = [
-						'chat_id' => $chat_id,
-						'first_request' => '<пусто>',
-						'second_request' => '<пусто>',
-						'third_request' => '<пусто>',
-						'fourth_request' => '<пусто>',
-						'fifth_request' => $reply
-					];
-					$db->insert('user_request_history', $data);
-				}
+				updateUserHistory($reply);
 			}			
 		}
 	}
--- url_handler.php
+++ url_handler.php
@@ -1,12 +1,5 @@
 <?php
-	const LOGIN =  'o_4dkf0dhc6p';
-	const API_KEY =  'R_a9dbe6c319fe4397946c86b8798b7abb';
-	const END_POINT =  'http://api.bit.ly/v3';
-	const HELP_REPLY = 'Данный бот предназначен для работы со ссылками. Он умеет сокращать или расшифровывать уже сокращённые ссылки. Для того, чтобы воспользоваться ботом, отправьте ему ссылку, которую необходимо сократить. Для просмотра последних действий, воспользуйтесь командой /history';
-	const DB_HOST = 'us-cdbr-iron-east-02.cleardb.net';
-	const DB_USER = 'b8ee931ac989b3';
-	const DB_PASS = '672ff549';
-	const DB_NAME = 'heroku_3fd1c3b404a32b0';
+	include('globals.php');
 	
 	function getShortUrl($longUrl):string {
         $url = "http://api.bit.ly/shorten?version=2.0.1&longUrl=$longUrl&login=o_4dkf0dhc6p&apiKey=R_a9dbe6c319fe4397946c86b8798b7abb&format=json&history=1";
