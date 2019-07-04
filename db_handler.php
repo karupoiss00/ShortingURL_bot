40aae5c7abcd73fb2f2b1f9dbc60c63dcd1f97ee
karupoiss00 <glizerin1108@gmail.com>
2019-07-04 11:24:02 +0300

    Функция getRow может возвращать null.

--- db_handler.php
+++ db_handler.php
@@ -69,7 +69,7 @@
 		return $res;
 	}
 	
-	function getRow(MysqliDb $db, $userId): array {
+	function getRow(MysqliDb $db, $userId): {
 		$db->where(CHAT_ID, $userId);
 		$row = $db->getOne('user_request_history');
 		return $row;
